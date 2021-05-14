e1258e5c7cdeda866656558445c97322cb30bd3d
sabe4441 <sambennetts54@gmail.com>
2021-05-14 17:17:01 -0600

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
             "fiona",
             "geopandas",
             "matplotlib",
-            "numpy"
+            "numpy",
             "shapely",
             "rasterstats",
             "scikit-learn",
